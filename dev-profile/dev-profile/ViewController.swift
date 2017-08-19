deae73ede0e9e7bff7d06f0a7552960a85448b7f
Christos Karolemeas <karolemeaschris@gmail.com>
2017-08-19 11:04:30 +0300

    Initial Commit

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Christos Karolemeas on 19/08/2017.
+//  Copyright © 2017 Christos Karolemeas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
